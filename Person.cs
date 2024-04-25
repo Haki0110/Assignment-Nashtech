fc8cb77bca3749c767fc70cae4673745eb1acb96
Haki0110 <thaiquochoang2002@gmail.com>
2024-04-25 22:34:17 +0700

    First commit

---
+++ Person.cs
@@ -0,0 +1,15 @@
+class Person{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Gender { get; set; }
+
+    public DateTime DateOfBirth { get; set; }
+
+    public string PhoneNumber { get; set; }
+
+    public string BirthPlace { get; set; }
+    public int Age { get; set; }
+
+    public string IsGraduated { get; set; }
+
+}   
